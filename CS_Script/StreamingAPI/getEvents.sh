909110039a1480a118f93212641a77f089bd24ed
Hideki Yamada <hideki9th@gmail.com>
2023-11-29 07:47:33 +0900

    Update getEvents.sh

--- CS_Script/StreamingAPI/getEvents.sh
+++ CS_Script/StreamingAPI/getEvents.sh
@@ -2,13 +2,18 @@
 
 ## Set Environment Variables
 source $1
+if [ -n "$CS_STREAM_OFFSET" ]; then
+    query_offset="&offset=${CS_STREAM_OFFSET}"
+fi
 
 ## Load functions
 eval "$(curl -s 'https://raw.githubusercontent.com/p-rex/cs-stream-logscale-conector/main/src/functions.sh')"
 
+
 ## Get OAuth2 Token
 log_msg "get oauth2 token"
 FALCON_API_BEARER_TOKEN=`getBearerToken`
+
 
 ## Get streaming URL
 log_msg "get streaming url"
@@ -18,6 +23,7 @@ dataFeedURL=$(echo $RESP_JSON | jq -r '.resources[].dataFeedURL' )
 dataFeedToken=$(echo $RESP_JSON | jq -r '.resources[].sessionToken.token' )
 dataFeedExpiration=$(echo $RESP_JSON | jq -r '.resources[].sessionToken.expiration' )
 refresh_active_session_url=$(echo $RESP_JSON | jq -r '.resources[0].refreshActiveSessionURL' )
+
 
 ## Start streaming
 log_msg "start streaming"
