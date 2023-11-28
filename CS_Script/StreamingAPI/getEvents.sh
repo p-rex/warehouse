89f4d2ab0d3f808a65af3e0c81ada417c8a9f973
Hideki Yamada <hideki9th@gmail.com>
2023-11-28 21:32:38 +0900

    Create getEvents.sh

---
+++ CS_Script/StreamingAPI/getEvents.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## Set Environment Variables
+source $1
+
+## Load functions
+eval "$(curl -s 'https://raw.githubusercontent.com/p-rex/cs-stream-logscale-conector/main/src/functions.sh')"
+
+## Get OAuth2 Token
+log_msg "get oauth2 token"
+FALCON_API_BEARER_TOKEN=`getBearerToken`
+
+## Get streaming URL
+log_msg "get streaming url"
+DATAFEED_URL="${CS_APIURL}/sensors/entities/datafeed/v2?format=json&appId=${RANDOM}"
+RESP_JSON=$(curl -sS -f -X GET -H "authorization: Bearer ${FALCON_API_BEARER_TOKEN}" $DATAFEED_URL )
+dataFeedURL=$(echo $RESP_JSON | jq -r '.resources[].dataFeedURL' )
+dataFeedToken=$(echo $RESP_JSON | jq -r '.resources[].sessionToken.token' )
+dataFeedExpiration=$(echo $RESP_JSON | jq -r '.resources[].sessionToken.expiration' )
+refresh_active_session_url=$(echo $RESP_JSON | jq -r '.resources[0].refreshActiveSessionURL' )
+
+
+log_msg "start streaming"
+curl -s -f -k -N -X GET ${dataFeedURL}${query_offset} -H "Accept: application/json" -H "Authorization: Token ${dataFeedToken}" | tee $LOG_FILE
