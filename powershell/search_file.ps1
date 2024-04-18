39479f66dd27ad79aad4b07d005d14715696e3a2
Hideki Yamada <hideki9th@gmail.com>
2024-04-18 13:20:40 +0900

    Create search_file.ps1

---
+++ powershell/search_file.ps1
@@ -0,0 +1,30 @@
+$targetDir = 'C:\'
+$targetFile = 'white.png'
+
+
+#status flg files
+$workDir = 'C:\Windows\Temp\'
+$status_flg_result = $workDir + 'cs_result.txt'
+$status_flg_done = $workDir + 'cs_done.txt'
+$status_flg_running = $workDir + 'cs_running.txt'
+
+
+# check already checked
+if((Test-Path $status_flg_done) -or (Test-Path $status_flg_running)){
+    exit
+}
+
+
+# Start
+echo '' > $status_flg_running
+$result = Get-ChildItem $targetDir -Recurse -File -Include $targetFile
+
+
+# Write result
+if($result){
+    echo $result > $status_flg_result
+}
+
+#Finish
+rm $status_flg_running
+echo '' > $status_flg_done
