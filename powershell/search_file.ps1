b486629ca6f940e3af9d99c248a7afb9d8167577
Hideki Yamada <hideki9th@gmail.com>
2024-04-18 14:38:13 +0900

    Update search_file.ps1

--- powershell/search_file.ps1
+++ powershell/search_file.ps1
@@ -1,30 +1,35 @@
-$targetDir = 'C:\'
-$targetFile = 'white.png'
+function searchFile($filename){
+
+    $targetDir = 'C:\'
+    $targetFile = $filename
 
 
-#status flg files
-$workDir = 'C:\Windows\Temp\'
-$status_flg_result = $workDir + 'cs_result.txt'
-$status_flg_done = $workDir + 'cs_done.txt'
-$status_flg_running = $workDir + 'cs_running.txt'
+    #status flg files
+    $workDir = 'C:\Windows\Temp\'
+    $status_flg_result = $workDir + 'cs_result.txt'
+    $status_flg_done = $workDir + 'cs_done.txt'
+    $status_flg_running = $workDir + 'cs_running.txt'
 
 
-# check already checked
-if((Test-Path $status_flg_done) -or (Test-Path $status_flg_running)){
-    exit
+    # check already checked
+    if((Test-Path $status_flg_done) -or (Test-Path $status_flg_running)){
+        exit
+    }
+
+
+    # Start
+    echo '' > $status_flg_running
+    $result = Get-ChildItem $targetDir -Recurse -File -Include $targetFile
+
+
+    # Write result
+    if($result){
+        echo $result > $status_flg_result
+    }
+
+    #Finish
+    rm $status_flg_running
+    echo '' > $status_flg_done
 }
 
-
-# Start
-echo '' > $status_flg_running
-$result = Get-ChildItem $targetDir -Recurse -File -Include $targetFile
-
-
-# Write result
-if($result){
-    echo $result > $status_flg_result
-}
-
-#Finish
-rm $status_flg_running
-echo '' > $status_flg_done
+searchFile($Args[0])
