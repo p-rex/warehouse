df497e15dd08f087d1c295fa0b79d0877ff345ac
Hideki Yamada <hideki9th@gmail.com>
2024-05-20 09:01:52 +0900

    Create helm_export.sh

---
+++ CS_Script/Cloud_Scripts/install/helm_export.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Create 12 environment variables
+# prerequsities
+# export FALCON_CLIENT_ID=XXXXXXXXXXXX
+# export FALCON_CLIENT_SECRET=XXXXXXXXXX
+# export FALCON_CID=XXXXXXXXX-XX
+
+
+
+if [ $FALCON_REGION = 'us-1' ]; then
+    export FALCON_CLOUD_API=api.crowdstrike.com
+elif [ $FALCON_REGION = 'us-2' ]; then
+    export FALCON_CLOUD_API=api.us-2.crowdstrike.com
+fi
+
+
+export FALCON_CONTAINER_REGISTRY=registry.crowdstrike.com
+
+export FALCON_CS_API_TOKEN=$(curl \
+--data "client_id=${FALCON_CLIENT_ID}&client_secret=${FALCON_CLIENT_SECRET}" \
+--request POST \
+--silent \
+https://${FALCON_CLOUD_API}/oauth2/token | jq -cr '.access_token | values')
+ 
+export FALCON_ART_USERNAME="fc-$(echo ${FALCON_CID} | awk '{ print tolower($0) }' | cut -d'-' -f1)"
+ 
+export FALCON_ART_PASSWORD=$(curl -X GET -H "authorization: Bearer ${FALCON_CS_API_TOKEN}" \
+https://${FALCON_CLOUD_API}/container-security/entities/image-registry-credentials/v1 \
+| jq -cr '.resources[].token | values')
+
+
+
+export FALCON_NAMESPACE=falcon-system
+export FALCON_IMAGE_SECRET=cs-registry-secret
+export FALCON_HELM_REPO=crowdstrike/falcon-sensor
