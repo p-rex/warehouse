9aaa53ea027cc94a0d57ff1ca2c85d1c6ab250ed
Hideki Yamada <hideki9th@gmail.com>
2024-08-21 23:07:29 +0900

    Create yahoo_finance.py

---
+++ CloudSampleApp/yahoo_finance/yahoo_finance.py
@@ -0,0 +1,17 @@
+import urllib.request
+import time
+
+# vars
+url = 'https://query1.finance.yahoo.com/v8/finance/chart/USDJPY=X?interval=1d'
+sleep_sec = 60
+
+
+while True:
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        decoded_body = body.decode('utf-8')
+        print(decoded_body)
+
+    time.sleep(10)
+
+
