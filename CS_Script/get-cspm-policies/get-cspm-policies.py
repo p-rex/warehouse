2e8dea11bce8d9e530dec40dd6bcbab26d13d04e
Hideki Yamada <hideki9th@gmail.com>
2024-12-09 10:10:12 +0900

    Create get-cspm-policies.py

---
+++ CS_Script/get-cspm-policies/get-cspm-policies.py
@@ -0,0 +1,46 @@
+from falconpy import CSPMRegistration
+import os
+import pandas as pd
+
+##---------------------------------------------------
+## functions
+##---------------------------------------------------
+
+def writeToCSV(data, filename):
+    df = pd.json_normalize(data)
+    df.to_csv(filename, index=False, encoding='utf-8')
+
+
+##---------------------------------------------------
+## main
+##---------------------------------------------------
+
+csv_filename = 'all_cspm_policies.csv'
+
+#Set API Credential
+falcon_client_id = os.environ['FALCON_CLIENT_ID']
+falcon_client_secret = os.environ['FALCON_CLIENT_SECRET']
+
+
+falcon = CSPMRegistration(client_id=falcon_client_id, client_secret=falcon_client_secret)
+
+
+# get all policy ids
+print("Get policy ids")
+all_policies = falcon.get_policy_settings()
+id_list = []
+for policy in all_policies['body']['resources']:
+    id_list.append(policy['policy_id'])
+
+
+# Get policy details
+print("Get policy details")
+all_policy_details = falcon.get_policy(ids=id_list)
+
+# Write to csv
+print("Write to csv")
+writeToCSV(all_policy_details['body']['resources'], csv_filename)
+
+# Done
+print('')
+print(f'{csv_filename} was created')
