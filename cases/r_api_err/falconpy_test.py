b974859c8c2f28fc442a070396f506223378f1bf
Hideki Yamada <hideki9th@gmail.com>
2025-06-04 17:31:13 +0900

    Create falconpy_test.py

---
+++ cases/r_api_err/falconpy_test.py
@@ -0,0 +1,11 @@
+from falconpy import ContainerImages
+import os
+
+
+falcon_client_id = os.environ['FALCON_CLIENT_ID']
+falcon_client_secret = os.environ['FALCON_CLIENT_SECRET']
+
+
+falcon = ContainerImages(client_id=falcon_client_id, client_secret=falcon_client_secret)
+resp = falcon.get_combined_images(sort="first_seen", limit=1)
+print(resp)
